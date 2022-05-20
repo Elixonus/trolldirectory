61947dcf7602655ea0bc9363cac5dbd341a54488
Daxonus <elias@dobrin.net>
2022-05-20 10:01:57 -0700

    troll program working i think

--- trolldirectory.py
+++ trolldirectory.py
@@ -1,25 +1,33 @@
 import os
+import glob
 import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
+from PIL import Image
 
 dirname = os.path.dirname(__file__)
-trollface - cv2.imread(os.path.join(dirname, "images/trollface.png"))
+
+
+def troll_directory(directory_path: str) -> None:
+    image_paths = []
+    for filename in glob.iglob(directory_path + "**/*.png", recursive=True):
+        image_paths.append()
+
 
 def troll_image(image_path: str) -> None:
     image = cv2.imread(image_path)
-    plt.imshow(image)
-    plt.show()
-
     faces, confidences = cv.detect_face(image)
-    # loop through detected faces and add bounding box
+    image = Image.fromarray(image)
     for face in faces:
-        startX, startY = face[0], face[1]
-        endX, endY = face[2], face[3]
-        # draw rectangle over face
-        cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
-    # display output
+        dx = int(0.2 * (face[2] - face[0]))
+        dy = int(0 * (face[3] - face[1]))
+        sx, sy = face[0] - dx, face[1] - dy
+        ex, ey = face[2] + dx, face[3] + dy
+        troll_face = Image.open(os.path.join(dirname, "images/trollface.png"))
+        troll_face = troll_face.resize((ex - sx, ey - sy))
+        image.paste(troll_face, (sx, sy), mask=troll_face)
     plt.imshow(image)
     plt.show()
 
-trollimage("couple.jpeg")
+
+troll_directory("")
